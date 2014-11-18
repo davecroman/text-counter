32608b101377627111f7f462321da9bcb353fb24
Armond Ave <aave@thoughtworks.com>
2014-11-19 10:55:00 +1100

    Combined interface and implementation into 1, since there is only one class implementing PersonService

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -1,13 +1,40 @@
 package com.example.service;
 
-
 import com.example.model.Person;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
-public interface PersonService {
+@Service
+public class PersonService {
+
+    @PersistenceContext
+    EntityManager em;
+        
+    @Transactional
+    public void addPerson(Person person) {
+        em.persist(person);
+    }
+
+    @Transactional
+    public List<Person> listPeople() {
+        CriteriaQuery<Person> c = em.getCriteriaBuilder().createQuery(Person.class);
+        Root<Person> from = c.from(Person.class);
+        c.orderBy(em.getCriteriaBuilder().asc(from.get("lastName")));
+        return em.createQuery(c).getResultList();
+    }
+
+    @Transactional
+    public void removePerson(Integer id) {
+        Person person = em.find(Person.class, id);
+        if (null != person) {
+            em.remove(person);
+        }
+    }
     
-    public void addPerson(Person person);
-    public List<Person> listPeople();
-    public void removePerson(Integer id);
 }
