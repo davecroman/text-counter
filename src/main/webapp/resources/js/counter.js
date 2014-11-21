793b9b5ff78759dd6f27439bee78390decab6688
Armond Ave <aave@thoughtworks.com>
2014-11-21 16:54:29 +1100

    Implemented the exclusion of non-alphanumeric characters from character count

--- src/main/webapp/resources/js/counter.js
+++ src/main/webapp/resources/js/counter.js
@@ -9,15 +9,17 @@ function countWords(text, excludeArticles) {
         }else{
             return words.length;
         }
-
     }else{
         return 0;
     }
 }
 
-function countCharacters(text, excludeSpaces) {
+function countCharacters(text, excludeSpaces, excludeNonAlphaNumeric) {
     if(excludeSpaces){
         text = text.replace(/\s+/g, "");
+    }
+    if(excludeNonAlphaNumeric){
+        text = text.replace(/\W+/g, "");
     }
     return text.length;
 }
@@ -28,10 +30,12 @@ function updateTable() {
     var characterCountField = document.getElementById("characterCount");
     var excludeArticles = $('.wordcount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude articles" }).length > 0;
     var excludeSpaces = $('.charactercount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude spaces"}).length > 0;
+    var excludeNonAlphaNumeric =  $('.charactercount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude non-alphanumeric"}).length > 0;
+
     var text = textArea.value;
 
     var wordCount = countWords(text, excludeArticles);
-    var charCount = countCharacters(text, excludeSpaces);
+    var charCount = countCharacters(text, excludeSpaces, excludeNonAlphaNumeric);
 
     wordCountField.textContent = wordCount;
     document.getElementById("clearTextButton").disabled = wordCount == 0;
