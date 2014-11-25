44e518e0f51e365455ebf14afa2711aa6d6e9481
Armond Ave <aave@thoughtworks.com>
2014-11-26 10:03:35 +1100

    Fixed issue #9 and counter.js

--- src/main/webapp/resources/js/counter.js
+++ src/main/webapp/resources/js/counter.js
@@ -1,52 +1,56 @@
-articles = ["a", "an", "the"];
+var articles = ["a", "an", "the"];
+var articleFilter = function(word){ return articles.indexOf(word) <= -1 };
 
 function countWords(text, excludeArticles) {
-    if( text.length > 0 ){
-        var words = text.trim().split(/\s+/g);
+    if( text.length == 0 ) { return 0; }
 
-        if( excludeArticles ){
-            return words.filter(function(word){ return articles.indexOf(word) <= -1 }).length;
-        }else{
-            return words.length;
-        }
+    var words = text.trim().split(/\s+/g);
+
+    if( excludeArticles ){
+        return words.filter(articleFilter).length;
     }else{
-        return 0;
+        return words.length;
     }
 }
 
 function countCharacters(text, excludeSpaces, excludeNonAlphaNumeric) {
+    var reducedText = text;
+
     if(excludeSpaces){
-        text = text.replace(/\s+/g, "");
+        reducedText = reducedText.replace(/\s+/g, "");
     }
     if(excludeNonAlphaNumeric){
-        text = text.replace(/\W+/g, "");
+        reducedText = reducedText.replace(/\W+/g, "");
     }
-    return text.length;
+    return reducedText.length;
 }
 
 function updateTable() {
     var textArea = document.getElementById("inputText");
     var wordCountField = document.getElementById("wordCount");
     var characterCountField = document.getElementById("characterCount");
-    var excludeArticles = $('.wordcount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude articles" }).length > 0;
-    var excludeSpaces = $('.charactercount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude spaces"}).length > 0;
-    var excludeNonAlphaNumeric =  $('.charactercount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude non-alphanumeric"}).length > 0;
 
     var text = textArea.value;
 
-    var wordCount = countWords(text, excludeArticles);
-    var charCount = countCharacters(text, excludeSpaces, excludeNonAlphaNumeric);
+    wordCountField.textContent = countWords(text, isExcludeArticlesChecked());
+    document.getElementById("clearTextButton").disabled = text.length == 0;
 
-    wordCountField.textContent = wordCount;
-    document.getElementById("clearTextButton").disabled = wordCount == 0;
+    characterCountField.textContent = countCharacters(text, isExcludeSpacesChecked(), isExcludeNonAlphaNumericChecked());
+}
 
-    characterCountField.textContent = charCount;
+function isExcludeSpacesChecked(){
+    return $('.charactercount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude spaces"}).length > 0;
+}
 
+function isExcludeNonAlphaNumericChecked(){
+    return $('.charactercount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude non-alphanumeric"}).length > 0;
+}
+
+function isExcludeArticlesChecked() {
+    return $('.wordcount-settings').dropdownCheckbox("checked").filter(function(e){ return e.label == "Exclude articles" }).length > 0;
 }
 
 function clearTextArea() {
-    var textArea = document.getElementById("inputText");
-
-    textArea.value = "";
+    document.getElementById("inputText").value = "";
     updateTable();
 }
