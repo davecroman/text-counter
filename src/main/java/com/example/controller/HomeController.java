50bf73109227fa8d2bf5e85a65de940ec437b3ad
Armond Ave <aave@thoughtworks.com>
2014-11-30 18:30:09 +1100

    Removed unused imports

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,12 +1,6 @@
 package com.example.controller;
 
-import com.example.model.Person;
-import com.example.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
